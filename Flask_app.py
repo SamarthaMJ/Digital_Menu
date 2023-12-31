c48cb46b97eabc099e6a8031947c8956acbcbfdb
Samartha MJ <76271058+SamarthaMJ@users.noreply.github.com>
2023-12-31 22:09:51 +0530

    Flask_app.py

--- Flask_app.py
+++ Flask_app.py
@@ -1,46 +1,36 @@
-from b2sdk.v1 import DownloadDestLocalFile
 from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
 import base64
-from b2sdk.v2 import *
 
-
-# Initialize B2 Cloud Storage
-b2_api = B2Api()
-application_key_id = 'fc58c8a67d36'
-application_key = '005e83fe4dfcc9c78e7c429652b67e23806fa30047'
-b2_api.authorize_account("production", application_key_id, application_key)
-bucket_name = 'sqlite3-data'
-bucket = b2_api.get_bucket_by_name(bucket_name)
 
 # Create a connection to the main database (or create it if it doesn't exist)
-# conn_main = sqlite3.connect('items_main.db')
-# cursor_main = conn_main.cursor()
-#
-# # Create a connection to the removed item's database (or create it if it doesn't exist)
-# conn_removed = sqlite3.connect('removed_items.db')
-# cursor_removed = conn_removed.cursor()
-#
-# # Create tables for main and removed items if they don't exist
-# cursor_main.execute('''
-#     CREATE TABLE IF NOT EXISTS items (
-#         id INTEGER PRIMARY KEY,
-#         name TEXT,
-#         image BLOB,
-#         price REAL
-#     )
-# ''')
-# conn_main.commit()
-#
-# cursor_removed.execute('''
-#     CREATE TABLE IF NOT EXISTS removed_items (
-#         id INTEGER PRIMARY KEY,
-#         name TEXT,
-#         image BLOB,
-#         price REAL
-#     )
-# ''')
-# conn_removed.commit()
+conn_main = sqlite3.connect('items_main.db')
+cursor_main = conn_main.cursor()
+
+# Create a connection to the removed item's database (or create it if it doesn't exist)
+conn_removed = sqlite3.connect('removed_items.db')
+cursor_removed = conn_removed.cursor()
+
+# Create tables for main and removed items if they don't exist
+cursor_main.execute('''
+    CREATE TABLE IF NOT EXISTS items (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        image BLOB,
+        price REAL
+    )
+''')
+conn_main.commit()
+
+cursor_removed.execute('''
+    CREATE TABLE IF NOT EXISTS removed_items (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        image BLOB,
+        price REAL
+    )
+''')
+conn_removed.commit()
 
 
 # Function to fetch all items from the main database
@@ -78,13 +68,6 @@ def index():
 
 @app.route('/add_item', methods=['GET', 'POST'])
 def add_item():
-    local_save_path = 'items_downloaded.db'
-    file_name = 'items_main.db'
-    bucket = b2_api.get_bucket_by_name(bucket_name)
-    download_dest = DownloadDestLocalFile(local_save_path)
-    bucket.download_file_by_name(file_name, download_dest)
-    print("Downloaded database")
-    delete_previous_files()
     conn_main = sqlite3.connect('items_main.db')
     cursor_main = conn_main.cursor()
     if request.method == 'POST':
@@ -98,21 +81,11 @@ def add_item():
             conn_main.commit()
 
             return redirect(url_for('index'))
-    upload_to_backblaze("items_main.db")
     return render_template('add_item.html')
 
 
 @app.route('/remove_item', methods=['GET', 'POST'])
 def remove_item():
-    local_save_path = 'items_downloaded.db'
-    file_name = 'removed_items.db'
-    file_name_2 = 'items_main.db'
-    bucket = b2_api.get_bucket_by_name(bucket_name)
-    download_dest = DownloadDestLocalFile(local_save_path)
-    bucket.download_file_by_name(file_name, download_dest)
-    bucket.download_file_by_name(file_name_2, download_dest)
-    print("Downloaded databases")
-    delete_previous_files()
     conn_removed = sqlite3.connect('removed_items.db')
     cursor_removed = conn_removed.cursor()
     conn_main = sqlite3.connect('items_main.db')
@@ -131,8 +104,6 @@ def remove_item():
             conn_removed.commit()
 
             return redirect(url_for('index'))
-    upload_to_backblaze("items_main.db")
-    upload_to_backblaze("removed_items.db")
     return render_template('remove_item.html')
 
 
@@ -156,15 +127,6 @@ def view_removed_items():
 
 @app.route('/restore_item', methods=['GET', 'POST'])
 def restore_item():
-    local_save_path = 'items_downloaded.db'
-    file_name = 'removed_items.db'
-    file_name_2 = 'items_main.db'
-    bucket = b2_api.get_bucket_by_name(bucket_name)
-    download_dest = DownloadDestLocalFile(local_save_path)
-    bucket.download_file_by_name(file_name, download_dest)
-    bucket.download_file_by_name(file_name_2, download_dest)
-    print("Downloaded databases")
-    delete_previous_files()
     conn_main = sqlite3.connect('items_main.db')
     cursor_main = conn_main.cursor()
     conn_removed = sqlite3.connect('removed_items.db')
@@ -184,25 +146,7 @@ def restore_item():
             conn_main.commit()
 
             return redirect(url_for('index'))
-    upload_to_backblaze("items_main.db")
-    upload_to_backblaze("removed_items.db")
     return render_template('restore_item.html')
-
-
-def upload_to_backblaze(file_name):
-    bucket.upload_local_file(local_file=file_name, file_name=file_name)
-    print(f"File '{file_name}' uploaded to Backblaze B2")
-
-
-def delete_previous_files():
-    try:
-        file_one = "Positive_Feedbacks.txt"
-        file_two = "Negative_Feedbacks.txt"
-        for version in bucket.list_file_versions(file_one and file_two):
-            bucket.delete_file_version(version.id_, version.file_name)
-        print("Previous files deleted successfully")
-    except Exception as e:
-        print("Error deleting previous files:", e)
 
 
 if __name__ == '__main__':
